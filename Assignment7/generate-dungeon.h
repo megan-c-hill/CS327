74f4bc26a1aebff1024f72f76285a9b22eb9abbd
megan-c-hill <meganchill@iastate.edu>
2019-03-27 10:19:42 -0500

    Init Commit for Assignment 7

---
+++ Assignment7/generate-dungeon.h
@@ -0,0 +1,10 @@
+#ifndef CS327_GENERATE_DUNGEON_H
+#define CS327_GENERATE_DUNGEON_H
+
+void generateRandomFloor(int numMonsters);
+
+void saveDungeon(char *fileName);
+
+void loadDungeon(char *fileName, int numMonsters);
+
+#endif //CS327_GENERATE_DUNGEON_H
