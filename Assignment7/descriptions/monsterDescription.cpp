445d57253338afd8ea7af528c6d1b3565f35aeb3
megan-c-hill <meganchill@iastate.edu>
2019-04-03 10:21:17 -0500

    Handle monsters that are incorrectly formatted

--- Assignment7/descriptions/monsterDescription.cpp
+++ Assignment7/descriptions/monsterDescription.cpp
@@ -31,17 +31,29 @@ Dice parseDice(string diceDescription) {
 	return Dice(base, dice, sides);
 }
 
-int parseLine(string basic_string, MonsterDescription *descr) {
+void parseLine(string basic_string, MonsterDescription *descr) {
 	unsigned long index = basic_string.find_first_of(' ');
-	if (index < 100) {
+	if (index < 100) { //If there's not another space index gives really long garbage, so 100 checks that
 		string keyword = basic_string.substr(0, index);
 		string data = basic_string.substr(index + 1);
 
 		if (keyword.compare("NAME") == 0) {
+			if (descr->assignedFields[0]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[0] = true;
 			strcpy(descr->name, data.c_str());
 		} else if (keyword.compare("SYMB") == 0) {
+			if (descr->assignedFields[7]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[7] = true;
 			descr->symbol = (char) data.at(0);
 		} else if (keyword.compare("COLOR") == 0) {
+			if (descr->assignedFields[2]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[2] = true;
 			unsigned long nextIndex = data.find_first_of(' ');
 			int count = 0;
 			while (nextIndex < 100 && count < 8) {
@@ -54,10 +66,22 @@ int parseLine(string basic_string, MonsterDescription *descr) {
 			enum colors c = colorMap.at(data.substr(0, nextIndex));
 			descr->color[count] = c;
 		} else if (keyword.compare("DAM") == 0) {
+			if (descr->assignedFields[6]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[6] = true;
 			descr->damage = parseDice(data);
 		} else if (keyword.compare("SPEED") == 0) {
+			if (descr->assignedFields[3]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[3] = true;
 			descr->speed = parseDice(data);
 		} else if (keyword.compare("ABIL") == 0) {
+			if (descr->assignedFields[4]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[4] = true;
 			unsigned long nextIndex = data.find_first_of(' ');
 			int count = 0;
 			while (nextIndex < 100 && count < 9) {
@@ -68,16 +92,21 @@ int parseLine(string basic_string, MonsterDescription *descr) {
 			}
 			strcpy(descr->abilities[count], data.substr(0, nextIndex).c_str());
 		} else if (keyword.compare("RRTY") == 0) {
+			if (descr->assignedFields[8]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[8] = true;
 			descr->rarity = stoi(data);
 		} else if (keyword.compare("HP") == 0) {
+			if (descr->assignedFields[5]) {
+				descr->fieldDoubleAssigned = true;
+			}
+			descr->assignedFields[5] = true;
 			descr->HP = parseDice(data);
 		} else {
-			cout << "Invalid" << keyword << endl;
-			return -1;
+			cout << "Invalid Keyword: " << keyword << endl;
 		}
 	}
-
-	return 1;
 }
 
 int readFile() {
@@ -106,14 +135,20 @@ int readFile() {
 				}
 
 				if (line.compare("DESC") == 0) {
+					if (md->assignedFields[1]) {
+						md->fieldDoubleAssigned = true;
+					}
+					md->assignedFields[1] = true;
 					int count = 0;
 					while (getline(myfile, line) && line.compare(".") != 0) {
 						strcpy(md->description[count], line.c_str());
-						count ++;
+						count++;
 					}
-				} else if(line.compare("BEGIN MONSTER") == 0){
+				} else if (line.compare("BEGIN MONSTER") == 0) {
 					md = new MonsterDescription();
-				} else if (parseLine(line, md) == 1) {
+				} else {
+					parseLine(line, md);
+
 
 				};
 			}
@@ -125,36 +160,46 @@ int readFile() {
 }
 
 MonsterDescription::MonsterDescription() {
+	for (int i = 0; i < 100; i++) {
+		strcpy(description[i], "NONE");
+	}
 	for (int i = 0; i < 8; i++) {
 		color[i] = 9;
 	}
 	for (int i = 0; i < 9; i++) {
 		strcpy(abilities[i], "NONE");
+		assignedFields[i] = false;
 	}
-	for (int i = 0; i < 100; i++) {
-		strcpy(description[i], "NONE");
-	}
+	fieldDoubleAssigned = false;
 }
 
 void MonsterDescription::print() {
-	cout << "NAME: " << name << endl;
-	cout << "DESCR: " << endl;
-	for (int i = 0; i < 100 && strcmp(description[i], "NONE") != 0; i++) {
-		cout << description[i] << endl;
+	bool allFieldsAssigned = true;
+	for (int i = 0; i < 9; i++) {
+		if(!assignedFields[i]) {
+			allFieldsAssigned = false;
+		}
 	}
-	cout << "COLOR: ";
-	for (int i = 0; i < 8 && color[i] != 9; i++) {
-		cout << color[i] << " ";
+	if(allFieldsAssigned && !fieldDoubleAssigned) {
+		cout << "NAME: " << name << endl;
+		cout << "DESCR: " << endl;
+		for (int i = 0; i < 100 && strcmp(description[i], "NONE") != 0; i++) {
+			cout << description[i] << endl;
+		}
+		cout << "COLOR: ";
+		for (int i = 0; i < 8 && color[i] != 9; i++) {
+			cout << color[i] << " ";
+		}
+		cout << endl;
+		cout << "SPEED: " << speed.print() << endl;
+		cout << "ABIL: ";
+		for (int i = 0; i < 8 && strcmp(abilities[i], "NONE") != 0; i++) {
+			cout << abilities[i] << " ";
+		}
+		cout << endl;
+		cout << "HP: " << HP.print() << endl;
+		cout << "DAM: " << damage.print() << endl;
+		cout << "SYMB: " << symbol << endl;
+		cout << "RRTY: " << to_string(rarity) << endl << endl;
 	}
-	cout << endl;
-	cout << "SPEED: " << speed.print() << endl;
-	cout << "ABIL: ";
-	for (int i = 0; i < 8 && strcmp(abilities[i], "NONE") != 0; i++) {
-		cout << abilities[i] << " ";
-	}
-	cout << endl;
-	cout << "HP: " << HP.print() << endl;
-	cout << "DAM: " << damage.print() << endl;
-	cout << "SYMB: " << symbol << endl;
-	cout << "RRTY: " << to_string(rarity) << endl << endl;
 }
