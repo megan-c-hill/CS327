6caea876d148faad179df0709cf8b0886941e492
megan-c-hill <meganchill@iastate.edu>
2019-04-07 17:58:35 -0500

    Ability to play game again

--- Assignment8/dungeon.cpp
+++ Assignment8/dungeon.cpp
@@ -16,56 +16,56 @@
 int main(int argc, char *argv[]) {
 	readMonsterFile();
 	readObjectFile();
-//	initscr();
-//	keypad(stdscr, true);
-//	curs_set(0);
-//	refresh();
-//	int seed = time(NULL);
-//	char seedText[20];
-//	mvaddstr(0, 0, seedText);
-//	refresh();
-//	srand(seed);
-//
-//	bool shouldSave = false;
-//	bool shouldLoad = false;
-//	bool shouldSetMonsters = false;
-//	int numMonsters = 10;
-//	char fileName[100] = "dungeon";
-//	initCharacterMap();
-//	initTeleportMap();
-//	initRememberedMap();
-//
-//	for (int argIndex = 1; argIndex < argc; argIndex++) {
-//		if (strcmp(argv[argIndex], "--save") == 0) {
-//			shouldSave = true;
-//		} else if (strcmp(argv[argIndex], "--load") == 0) {
-//			shouldLoad = true;
-//		} else if (strcmp(argv[argIndex], "--nummon") == 0) {
-//			shouldSetMonsters = true;
-//		} else {
-//			if (shouldSetMonsters) {
-//				numMonsters = (int) strtol(argv[argIndex], (char **) NULL, 10);
-//			} else {
-//				strcpy(fileName, argv[argIndex]);
-//			}
-//		}
-//	}
-//	fogOfWarActivated = true;
-//
-//	if (shouldLoad) {
-//		loadDungeon(fileName, numMonsters);
-//	} else {
-//		generateRandomFloor(numMonsters);
-//	}
-//	if (shouldSave) {
-//		saveDungeon(fileName);
-//	}
-//
-//	free(rooms);
-//	free(upStairs);
-//	free(downStairs);
-//
-//	playGame();
-//	endwin();
+	initscr();
+	keypad(stdscr, true);
+	curs_set(0);
+	refresh();
+	int seed = time(NULL);
+	char seedText[20];
+	mvaddstr(0, 0, seedText);
+	refresh();
+	srand(seed);
+
+	bool shouldSave = false;
+	bool shouldLoad = false;
+	bool shouldSetMonsters = false;
+	int numMonsters = 10;
+	char fileName[100] = "dungeon";
+	initCharacterMap();
+	initTeleportMap();
+	initRememberedMap();
+
+	for (int argIndex = 1; argIndex < argc; argIndex++) {
+		if (strcmp(argv[argIndex], "--save") == 0) {
+			shouldSave = true;
+		} else if (strcmp(argv[argIndex], "--load") == 0) {
+			shouldLoad = true;
+		} else if (strcmp(argv[argIndex], "--nummon") == 0) {
+			shouldSetMonsters = true;
+		} else {
+			if (shouldSetMonsters) {
+				numMonsters = (int) strtol(argv[argIndex], (char **) NULL, 10);
+			} else {
+				strcpy(fileName, argv[argIndex]);
+			}
+		}
+	}
+	fogOfWarActivated = true;
+
+	if (shouldLoad) {
+		loadDungeon(fileName, numMonsters);
+	} else {
+		generateRandomFloor(numMonsters);
+	}
+	if (shouldSave) {
+		saveDungeon(fileName);
+	}
+
+	free(rooms);
+	free(upStairs);
+	free(downStairs);
+
+	playGame();
+	endwin();
 
 }
