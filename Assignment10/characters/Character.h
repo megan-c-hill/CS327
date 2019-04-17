5469a0cb3c2e994ec951cb129bb95ba9231d68f0
megan-c-hill <meganchill@iastate.edu>
2019-04-17 13:40:36 -0500

    Init commit for Assignment 10

---
+++ Assignment10/characters/Character.h
@@ -0,0 +1,41 @@
+#ifndef CS327_CHARACTER_H
+#define CS327_CHARACTER_H
+
+#include <stdint.h>
+#include "../descriptions/dice.h"
+#include "./Object.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Character {
+public:
+	char name[100];
+	char description[100][78];
+	char symbol;
+	int color;
+	int HP;
+	vector<Dice> damage;
+	uint8_t speed;
+	uint8_t x;
+	uint8_t y;
+	void displayCharacter();
+	int getDamage();
+};
+
+void initMaps();
+
+void initRememberedMap();
+
+void moveToSpot(Character *character, int newX, int newY);
+
+int displayMonsterList(int offset, Character *player);
+
+void tunnel(Character *character, int newX, int newY);
+
+void playGame();
+
+//void useMap(Character *character, DistancePosition distanceMap[TOTAL_HEIGHT][TOTAL_WIDTH]);
+
+#endif //CS327_CHARACTER_H
---
+++ Assignment10/characters/Player.h
@@ -0,0 +1,30 @@
+#ifndef CS327_PLAYER_H
+#define CS327_PLAYER_H
+
+#include "Character.h"
+
+class Player : public Character {
+public:
+	Object* equipment[12];
+	Object* inventory[10];
+	void showInventory();
+	void showEquipment();
+	void displayInventory();
+	void displayEquipment();
+	void wearItem();
+	void takeOffEquipment();
+	void expungeItem();
+	void dropItem();
+	void inspectItem();
+};
+
+Player *generatePlayerCharacter();
+
+void placePlayer();
+
+void placePlayerWithCoords(int x, int y);
+
+int playerMove(Player* player);
+
+
+#endif //CS327_PLAYER_H
